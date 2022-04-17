d0e6cedd2cb8ac0888e7cbe8adb2f01f1ce9eb13
Jennifer Taylor <dragonminded@dragonminded.com>
2022-04-17 22:32:49 +0000

    Get rid of endianness handling that we won't need for this port.

--- src/m_swap.h
+++ src/m_swap.h
@@ -29,17 +29,8 @@
 #endif
 
 
-// Endianess handling.
-// WAD files are stored little endian.
-#ifdef __BIG_ENDIAN__
-short	SwapSHORT(short);
-long	SwapLONG(long);
-#define SHORT(x)	((short)SwapSHORT((unsigned short) (x)))
-#define LONG(x)         ((long)SwapLONG((unsigned long) (x)))
-#else
 #define SHORT(x)	(x)
 #define LONG(x)         (x)
-#endif
 
 
 
